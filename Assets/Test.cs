fa26a78857fe64e58526c1492faff5adebf7237a
Rodrigo Sonego <rodrigoasonego@gmail.com>
2022-11-17 16:08:56 -0300

    refletindo a curva quando bate no prisma

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -19,30 +19,35 @@ public class Test : MonoBehaviour
 		Gizmos.color = Color.red;
 		Gizmos.DrawSphere(transform.position, 0.05f);
 
-		Gizmos.color = Color.white;
+		Gizmos.color = Color.white;	
 
-		//float xScale = origin.localScale.x * 0.5f;
+		RenderSpriral(origin.transform.position, 0, isClockwise: true);
+	}
 
+	void RenderSpriral(Vector3 origin, int initialIndex, bool isClockwise)
+    {
 		Vector2 pos2D = new Vector2(transform.position.x, transform.position.z);
-		Vector2 originPos2D = new Vector2(origin.position.x, origin.position.z);
+		Vector2 originPos2D = new Vector2(origin.x, origin.z);
+
+		int clockwiseModifier = isClockwise ? 1 : -1;
 
 		float ang2 = Mathf.Atan2(originPos2D.x, originPos2D.y);
 		print($"-> {ang2}");
 
-		float dist = Vector2.Distance(pos2D, originPos2D);
-
 		float startAng = -ang2 + TAU * 0.25f;
 
+		float dist = Vector2.Distance(pos2D, originPos2D);
+
 		int max = 100;
-		for (int i = 0; i < max; i++)
+		for (int i = 0; i < max - initialIndex; i++)
 		{
-			var ang = startAng + (i / (float)(max-1)) * TAU;
-			float unit = i / (float) max;
+			var ang = startAng + (i / (float)(max - 1)) * TAU * clockwiseModifier;
+			float unit = i / (float)max;
 
 			// ax + b
-			Vector3 offset = new Vector3(Mathf.Cos(ang) * dist, (unit * slope) + origin.transform.position.y , Mathf.Sin(ang) * dist);
+			Vector3 offset = new Vector3(Mathf.Cos(ang) * dist, (unit * slope) + origin.y, Mathf.Sin(ang) * dist);
 
-			var rotatedVector = Quaternion.AngleAxis(90, Vector3.down) * offset;
+			var rotatedVector = Quaternion.AngleAxis(90, Vector3.down * clockwiseModifier) * offset;
 			var hasHit = Physics.Raycast(transform.position + offset, rotatedVector.normalized * 0.15f, 0.15f, LayerMask.NameToLayer("Prism")); ;
 
 			Gizmos.color = hasHit ? Color.red : Color.white;
@@ -51,7 +56,11 @@ public class Test : MonoBehaviour
 			Gizmos.color = hasHit ? Color.red : Color.blue;
 			Gizmos.DrawRay(transform.position + offset, rotatedVector.normalized * 0.15f);
 
+			if (hasHit)
+            {
+				RenderSpriral(transform.position + offset, i, !isClockwise);
+				break;
+            }
 		}
-		
 	}
 }
