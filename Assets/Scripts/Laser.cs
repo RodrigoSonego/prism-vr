b34bafc75eed85045cd042c6963414431dbddd1c
Rodrigo Sonego <rodrigoasonego@gmail.com>
2022-11-18 17:37:04 -0300

    line rendererzao e checagem de colisao menos macaco

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -7,9 +7,17 @@ public class Laser : MonoBehaviour
 	const float TAU = 6.28318530f;
 
 	[SerializeField] private Transform origin;
+	[SerializeField] private LineRenderer lineRenderer;
+
+	[SerializeField] private int maxPoints = 100;
 
 	[SerializeField] private float slope = 0f;
 	[SerializeField] private float height = 1.5f;
+
+	void Start()
+	{
+		//RenderSpriral(origin.transform.position, 0, isClockwise: true);
+	}
 
 	void OnDrawGizmos()
 	{
@@ -19,48 +27,52 @@ public class Laser : MonoBehaviour
 		Gizmos.color = Color.red;
 		Gizmos.DrawSphere(transform.position, 0.05f);
 
-		Gizmos.color = Color.white;	
+		Gizmos.color = Color.white;
 
+		lineRenderer.positionCount = maxPoints;
 		RenderSpriral(origin.transform.position, 0, isClockwise: true);
 	}
 
 	void RenderSpriral(Vector3 origin, int initialIndex, bool isClockwise)
-    {
+	{
 		Vector2 pos2D = new Vector2(transform.position.x, transform.position.z);
 		Vector2 originPos2D = new Vector2(origin.x, origin.z);
 
 		int clockwiseModifier = isClockwise ? 1 : -1;
 
 		float ang2 = Mathf.Atan2(originPos2D.x, originPos2D.y);
-		print($"-> {ang2}");
 
 		float startAng = -ang2 + TAU * 0.25f;
 
 		float dist = Vector2.Distance(pos2D, originPos2D);
 
-		int max = 100;
-		for (int i = 0; i < max - initialIndex; i++)
+		for (int i = 0; i < maxPoints - initialIndex; i++)
 		{
-			var ang = startAng + (i / (float)(max - 1)) * TAU * clockwiseModifier;
-			float unit = i / (float)max;
+			var ang = startAng + (i / (float)(maxPoints - 1)) * TAU * clockwiseModifier;
+			float unit = i / (float)maxPoints;
+
+			int positionIndex = i + initialIndex;
 
 			// ax + b
 			Vector3 offset = new Vector3(Mathf.Cos(ang) * dist, (unit * slope) + origin.y, Mathf.Sin(ang) * dist);
 
-			var rotatedVector = Quaternion.AngleAxis(90, Vector3.down * clockwiseModifier) * offset;
-			var hasHit = Physics.Raycast(transform.position + offset, rotatedVector.normalized * 0.15f, 0.15f, LayerMask.NameToLayer("Prism")); ;
+			Vector3 pointPosition = transform.position + offset;
 
-			Gizmos.color = hasHit ? Color.red : Color.white;
-			Gizmos.DrawSphere(transform.position + offset, 0.05f);
+			bool hasHit = false;
+			if(positionIndex > 0)
+            {
+				Vector3 directionToCurrent = pointPosition - lineRenderer.GetPosition(positionIndex - 1);
+				hasHit = Physics.Raycast(pointPosition, directionToCurrent.normalized, 0.15f, LayerMask.NameToLayer("Prism")); ;
+            }
 
-			Gizmos.color = hasHit ? Color.red : Color.blue;
-			Gizmos.DrawRay(transform.position + offset, rotatedVector.normalized * 0.15f);
+			lineRenderer.SetPosition(positionIndex, pointPosition);
 
 			if (hasHit)
-            {
-				RenderSpriral(transform.position + offset, i, !isClockwise);
+			{
+				Vector3 lastPosition = lineRenderer.GetPosition(positionIndex - 1);
+				RenderSpriral(lastPosition, positionIndex+1, !isClockwise);
 				break;
-            }
+			}
 		}
 	}
 }
